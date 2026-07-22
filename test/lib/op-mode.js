7ab749cbe08a8da4d798e7a4e97a35a0a04aa211
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2026-07-23 00:43:52 +0200

    Emit run-started and run-failed events from op mode run

--- test/lib/op-mode.js
+++ test/lib/op-mode.js
@@ -273,6 +273,63 @@ describe('OpMode', function () {
                 done();
             });
         });
+
+        it('emits run-started with the op server before stopping anything', function (done) {
+            var op = fakeServer('op', true);
+            var other = fakeServer('other', true);
+            var opMode = opModeWith([op, other]);
+
+            var startedWith = null;
+            opMode.on('run-started', function (server) {
+                startedWith = server;
+                // Nothing has been stopped yet at the moment run-started fires.
+                other.stops.should.eql(0);
+                op.restarts.should.eql(0);
+            });
+
+            opMode.run(function () {
+                startedWith.should.equal(op);
+                done();
+            });
+        });
+
+        it('emits run-failed with the op server and error when the restart fails', function (done) {
+            var op = fakeServer('op', true);
+            op.restart = function (cb) {
+                cb(new Error('did not stop in time'));
+            };
+            var opMode = opModeWith([op]);
+
+            var failedWith = null;
+            opMode.on('run-failed', function (server, err) {
+                failedWith = {server: server, err: err};
+            });
+
+            opMode.run(function (err) {
+                err.should.be.an.Error();
+                failedWith.server.should.equal(op);
+                failedWith.err.message.should.eql('did not stop in time');
+                done();
+            });
+        });
+
+        it('does not emit run-started when a guard refuses the run', function (done) {
+            var other = fakeServer('other', true);
+            // opServerId 'op' but no such server in the manager: run() refuses before touching anything.
+            var opMode = opModeWith([other]);
+
+            var started = false;
+            opMode.on('run-started', function () {
+                started = true;
+            });
+
+            opMode.run(function (err) {
+                err.should.be.an.Error();
+                started.should.be.false();
+                other.stops.should.eql(0);
+                done();
+            });
+        });
     });
 
     describe('tick()', function () {
